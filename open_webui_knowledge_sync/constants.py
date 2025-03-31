76d1c39a8e77fd4f27114221ef98534f47c7a02b
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-03-31 16:54:18 +0300

    more file exclusions

--- open_webui_knowledge_sync/constants.py
+++ open_webui_knowledge_sync/constants.py
@@ -1,18 +1,24 @@
 """open_webui_knowledge_sync/constants.py"""
 
 EXCLUDES = [
+    r".*",
     r".*/",
+    r"*~",
     r"__pycache__/",
     r"build/",
     r"dist/",
     r"wheels/",
     r"*.egg-info/",
     r".venv/",
-    r"**/.*",
     r"*.py[oc]",
-    r"**/*.lock",
-    r"**/*.tmp",
-    r"*~",
+    r"*.lock",
+    r"*.tmp",
+    r"*.typed",
+    r"*.gz",
+    r"*.zip",
+    r"*.rar",
+    r"*.7z",
+    r"*.whl",
 ]
 
 INCLUDES = [
