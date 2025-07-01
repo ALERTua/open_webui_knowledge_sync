71ca4102ce768f4a2ee78f1a1b7e5b2cc602149e
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-07-01 16:17:20 +0300

    chore: Update sync script to preserve original directory
    - Execute synchronization script from working directory, preserving original directory.

--- sync.cmd
+++ sync.cmd
@@ -1,3 +1,11 @@
 
 @echo off
-uv run owui_sync %*
+set CURDIR=%CD%
+echo curdir %CURDIR%
+
+pushd %~dp0
+    echo jumped to %CD%
+    echo running uv run --directory "%CURDIR%" owui_sync %*
+    uv run --directory "%CURDIR%" owui_sync %*
+popd
+echo returned to %CD%
