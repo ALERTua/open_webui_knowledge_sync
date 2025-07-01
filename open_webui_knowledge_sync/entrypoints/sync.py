81752b97c37d82585fbd208fc459c0a3c04c7c9c
Alexey ALERT Rubashёff <alexey.rubasheff@gmail.com>
2025-07-01 16:21:43 +0300

    refactor: Improve error handling and logging in sync process
    - Log debug messages when working with paths and processing files, and specify error messages for failed file uploads and adding files to knowledge.

--- open_webui_knowledge_sync/entrypoints/sync.py
+++ open_webui_knowledge_sync/entrypoints/sync.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import logging
 from pathlib import Path  # noqa: TC003
 
 from typing import Annotated
@@ -12,12 +13,13 @@ from open_webui_knowledge_sync.backends.filesystem import get_filtered_files
 from open_webui_knowledge_sync.backends.openwebui.knowledge import OpenWebUIKnowledgeAPI
 from open_webui_knowledge_sync.backends.openwebui.files import OpenWebUIFilesAPI
 
+LOG = logging.getLogger(__name__)
 app = typer.Typer(add_completion=False)
 
 
 # noinspection PyShadowingBuiltins
 @app.command(no_args_is_help=True)
-def sync(  # noqa: C901
+def sync(  # noqa: C901,PLR0912
     path: Annotated[
         list[Path] | None,
         typer.Argument(
@@ -62,6 +64,8 @@ def sync(  # noqa: C901
         typer.echo("No path provided")
         raise typer.Abort
 
+    LOG.debug(f"working with poath: {path}")
+
     owk = OpenWebUIKnowledgeAPI(url=url, token=token)
     knowledge = owk.get_knowledge(knowledge)
     if knowledge is None:
@@ -73,10 +77,21 @@ def sync(  # noqa: C901
     files: list[Path] = []
     for p in path:
         if p.is_file():
+            LOG.error(f" file: {p}")
             files.append(p)
         elif p.is_dir():
-            files.extend(get_filtered_files(p))
+            LOG.error(f" dir: {p}")
+            dir_files = get_filtered_files(p)
+            files.extend(dir_files)
 
+    # filter zero size files
+    files = [f for f in files if f.stat().st_size > 0]
+
+    if not files:
+        typer.echo("No files found")
+        raise typer.Exit(code=1)
+
+    LOG.debug(f"Files:\n{'\n'.join([str(f) for f in files])}")
     owf = OpenWebUIFilesAPI(url=url, token=token)
     uploaded = False
     typer.echo(f"Processing {len(files)} files...")
@@ -87,7 +102,11 @@ def sync(  # noqa: C901
             if upload_result:
                 file_id = owf.upload_file(file_)
                 if file_id:
-                    owk.add_file_to_knowledge_by_id(knowledge_id, file_id)
+                    response = owk.add_file_to_knowledge_by_id(knowledge_id, file_id)
+                    if not response:
+                        typer.echo(f" {file_} adding to knowledge failed")
+                        continue
+
                     uploaded = True
                 else:
                     typer.echo(f" {file_} upload failed")
